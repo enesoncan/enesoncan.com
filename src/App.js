fb7f8af47f98646cf0bf1e4e5f8caa0ff1bf3f9c
Enes Öncan <enes.oncan@cimri.com>
2020-02-23 00:02:12 +0300

    Initialize font family and texts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello, I'm Enes.</h1>
+      <p>I work as a Front-End Developer at Cimri.com</p>
     </div>
   );
 }
