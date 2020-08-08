5c2076d2bd7e4b94703c3fe8ac35053c37371560
Enes Öncan <enes.oncan@cimri.com>
2020-08-08 17:10:34 +0300

    v0.5.0 Added localstorage for dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './containers/Home';
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(false);
 
+  useEffect(() => {
+    let value = localStorage.getItem('dark-mode-enabled');
+    if (value === null) {
+      setDarkTheme(false);
+      window.localStorage.setItem('dark-mode-enabled', false);
+    } else if (value === 'true') {
+      setDarkTheme(true);
+    } else {
+      setDarkTheme(false);
+    }
+  }, []);
+
   const toggleTheme = () => {
-    setDarkTheme(!darkTheme);
+    if (localStorage.getItem('dark-mode-enabled') == 'false') {
+      setDarkTheme(true);
+      window.localStorage.setItem('dark-mode-enabled', true);
+    } else {
+      setDarkTheme(false);
+      window.localStorage.setItem('dark-mode-enabled', false);
+    }
   };
 
   return (
-    <div className={darkTheme ? 'darkTheme' : 'lightTheme'}>
+    <div
+      className={localStorage.getItem('dark-mode-enabled') === 'true' ? 'darkTheme' : 'lightTheme'}
+    >
       <Home darkTheme={darkTheme} toggleTheme={toggleTheme} />
     </div>
   );
